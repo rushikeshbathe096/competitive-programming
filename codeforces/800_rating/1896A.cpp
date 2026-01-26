0b31378b76fde42ca0b146665166eee4e95d855b
rushikeshbathe096 <rushikeshbathe096@gmail.com>
2026-01-26 18:56:03 +0530

    Added boilerplate code

---
+++ codeforces/800_rating/1896A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+#define endl '\n'
+#define int long long
+typedef long long ll;
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+
+typedef vector<int> vi;
+typedef set<int> st;
+typedef unordered_set<int> ust;
+typedef map<int,int> mp;
+typedef unordered_map<int,int> ump;
+typedef pair<int,int> p;
+
+void solve() {
+    
+}
+
+int32_t main() {
+    fastio;
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
