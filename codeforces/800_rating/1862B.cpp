b9020441fee66327ce7a257e211a6a4233ac1c65
rushikeshbathe096 <rushikeshbathe096@gmail.com>
2026-01-30 00:16:03 +0530

    sequence game

--- codeforces/800_rating/1862B.cpp
+++ codeforces/800_rating/1862B.cpp
@@ -1,3 +1,24 @@
+/*
+Problem: B. Sequence Game
+
+Description:
+Vika forms sequence b from sequence a by writing a1, then writing ai (i>1) only if ai-1 <= ai. Given b, construct any valid sequence a that produces b.The length of a must satisfy n <= m <= 2n.
+
+Input:
+t test cases.
+For each test case:
+- Integer n: length of b
+- n integers b1...bn
+
+Output:
+For each test case:
+- Integer m: length of a, m integers a1...am
+
+Approach:
+Append the first element of b to a.For each next element, compare it with the previous element in b:
+- If it is greater than or equal, append it to a
+- Otherwise, append 1 and then append the current element
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -16,31 +37,26 @@ typedef map<int,int> mp;
 typedef unordered_map<int,int> ump;
 typedef pair<int,int> p;
 
-void solve() {
+void solve(){
     int n;
-        cin >> n;
-        vector<long long> b(n);
-        for (int i = 0; i < n; i++) {
-            cin >> b[i];
+    cin>>n;
+    vi b(n);
+    for(int i=0;i<n;i++){
+        cin>>b[i];
+    }
+    vi a;
+    a.pb(b[0]);
+    for(int i=1;i<n;i++){
+        if(b[i]>=b[i-1])a.pb(b[i]);
+        else{
+            a.pb(1);
+            a.pb(b[i]);
         }
- 
-        vector<long long> a;
-        a.push_back(b[0]);
- 
-        for (int i = 1; i < n; i++) {
-            if (b[i - 1] <= b[i]) {
-                a.push_back(b[i]);
-            } else {
-                a.push_back(1);   // force a drop
-                a.push_back(b[i]);
-            }
-        }
- 
-        cout << a.size() << "\n";
-        for (long long x : a) {
-            cout << x << " ";
-        }
-        cout << "\n";
+    }
+    for(int i:a){
+        cout<<i<<" ";
+    }
+    cout<<endl;
 }
 
 int32_t main() {
