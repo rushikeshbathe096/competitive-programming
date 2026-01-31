b28ecf66a8d4c418e7e94662dc14346298dce372
rushikeshbathe096 <rushikeshbathe096@gmail.com>
2026-02-01 02:28:24 +0530

    Coins

--- codeforces/800_rating/1814A.cpp
+++ codeforces/800_rating/1814A.cpp
@@ -13,3 +13,37 @@ For each test case:- Print "YES" if it is possible to represent n burles using c
 Approach:
 If k is odd, the answer is always YES since any parity can be adjusted using 2-burle coins; if k is even, n must be even because both coin denominations are even.
 */
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+#define endl '\n'
+#define int long long
+typedef long long ll;
+#define pb push_back
+#define all(v) v.begin(), v.end()
+ 
+typedef vector<int> vi;
+typedef set<int> st;
+typedef unordered_set<int> ust;
+typedef map<int,int> mp;
+typedef unordered_map<int,int> ump;
+typedef pair<int,int> p;
+ 
+void solve() {
+    int n,k;
+    cin >> n>>k;
+    if(k&1)cout<<"Yes"<<endl;
+    else cout<<(n&1?"No":"Yes")<<endl;
+}
+ 
+ 
+int32_t main() {
+    fastio;
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+ 
+    return 0;
+}
