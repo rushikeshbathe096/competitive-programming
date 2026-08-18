e624bb06bbf9bcc53e2cb2e2b4706576fdb15f7f
rushikeshbathe096 <rushikeshbathe096@gmail.com>
2026-08-19 01:33:40 +0530

    1789

---
+++ codeforces/800_rating/1789A.cpp
@@ -0,0 +1,103 @@
+/*
+Problem: A. Serval and Mocha's Array
+
+Description:
+Given an array of positive integers, check if we can reorder it such that the array becomes beautiful.
+
+Approach:
+A beautiful array must have its prefix of length 2 as good.So, we only need to find any two elements whose gcd is <= 2.
+We check every pair:
+- If gcd(a[i], a[j]) <= 2 -> YES
+- If no such pair exists -> NO
+
+TC: O(n² log A)
+We check O(n²) pairs and each gcd takes O(log A).
+
+SC: O(1)
+Only constant extra variables are used.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+
+#define int long long
+#define ll long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define endl '\n'
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+
+typedef set<int> st;
+typedef unordered_set<int> ust;
+
+typedef map<int,int> mp;
+typedef unordered_map<int,int> ump;
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX;
+
+int binexp(int a, int b){
+    a %= MOD;
+    int ans = 1;
+
+    while(b){
+        if(b & 1)
+            ans = (ans * a) % MOD;
+
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+
+    return ans;
+}
+
+bool isPrime(int n){
+    if(n < 2) return false;
+
+    for(int i = 2; i * i <= n; i++){
+        if(n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    vi a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(__gcd(a[i],a[j])<=2){
+                cout<<"YES"<<endl;
+                return;
+            }
+        }
+    }
+    cout<<"NO"<<endl;
+}
+
+int32_t main(){
+    fastio;
+
+    int t = 1;
+    cin >> t;
+
+    while(t--)
+        solve();
+
+    return 0;
+}
